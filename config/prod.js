87f572ad476ff6014bd27a050c8af01c420fa827
Dmitriy <dmitriygrigorash90@gmail.com>
2020-02-05 22:14:36 +0200

    config

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,5 @@
 module.exports = {
     mongoUri: process.env.MONGO_URI,
-    redirectDomain: process.env.REDIRECT_DOMAIN,
     aKey: process.env.A_KEY,
     tKey: process.env.T_KEY,
 };
-
-//TODO: 1: generate and set new aKey for .env prod variable
-//TODO: 2: set others variables on heroku environment
